e659d200858163762d66996461645035b1d360d6
Blake Lundquist <lundqub@outlook.com>
2020-01-16 12:28:08 -0800

    install react router

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  /*
+    Wrap our App component within the BrowserRouter component
+    to get access to routing features in all child components
+  */
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
