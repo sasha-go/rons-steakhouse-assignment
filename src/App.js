10b8cabc5d638fa5011e370e5900774c8bf5329b
sasha-go <sasha.lfg@gmail.com>
2020-02-18 20:43:42 -0800

    added NotFound Page routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import Nav from './Nav/Nav'
 import Header from './Header/Header'
 import Footer from './Footer/Footer'
 import HomePage from './HomePage/HomePage'
 import MenuPage from './MenuPage/MenuPage'
 import BioPage from './BioPage/BioPage'
+import NotFoundPage from './NotFoundPage/NotFoundPage'
 import './App.css'
 
 
@@ -20,9 +21,12 @@ export default class App extends Component {
           <Header />
         </header>
         <main>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/menu' component={MenuPage} />
-          <Route path='/bio' component={BioPage} />
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/menu' component={MenuPage} />
+            <Route path='/bio' component={BioPage} />
+            <Route component={NotFoundPage} />
+          </Switch>
         </main>
         <footer>
           <Footer />
